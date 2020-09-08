5edadf684597ca59f0795dfe50287e0faee38803
WILDGARO <you@example.comnawamin0322@gmail.com>
2020-09-08 14:26:39 +0700

    Center222

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Container(
-          color: Colors.red,
+          color: Colors.black,
           child: TestWidgt(),
           height: 800.00,
           width: 500.00,
@@ -25,40 +25,13 @@ class MyApp extends StatelessWidget {
 class TestWidgt extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.bottomCenter,
-      children: [
-        Container(
-          height: 300.0,
-          width: 300.0,
-          color: Colors.purple,
-        ),
-        Container(
-          height: 200.0,
-          width: 200.0,
-          color: Colors.blue,
-        ),
-        Container(
-          height: 100.0,
-          width: 100.0,
-          color: Colors.green,
-        ),
-        Container(
-          height: 50.0,
-          width: 50.0,
-          color: Colors.yellow,
-        ),
-        Container(
-          height: 30.0,
-          width: 30.0,
-          color: Colors.orange,
-        ),
-        
-      ]
+    return Center(
+      child: Container(
+        height: 200.0,
+        width: 200.0,
+        color: Colors.red,
+      )
     );
-
-      
-      
-
+   
   }
 }
