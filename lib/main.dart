059695c7b89a0b2a90e15e4a14732dfd9fd520b4
WILDGARO <you@example.comnawamin0322@gmail.com>
2020-09-08 14:19:55 +0700

    Center

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,37 @@ class MyApp extends StatelessWidget {
 class TestWidgt extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      "https://media3.giphy.com/media/Dl2seYrwPvfjO/giphy.gif");
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        Container(
+          height: 300.0,
+          width: 300.0,
+          color: Colors.purple,
+        ),
+        Container(
+          height: 200.0,
+          width: 200.0,
+          color: Colors.blue,
+        ),
+        Container(
+          height: 100.0,
+          width: 100.0,
+          color: Colors.green,
+        ),
+        Container(
+          height: 50.0,
+          width: 50.0,
+          color: Colors.yellow,
+        ),
+        Container(
+          height: 30.0,
+          width: 30.0,
+          color: Colors.orange,
+        ),
+        
+      ]
+    );
 
       
       
